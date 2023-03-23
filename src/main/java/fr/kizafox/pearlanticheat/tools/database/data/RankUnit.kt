62129e09a24003c14fb9f9b420870b7a007a6fc3
KIZAFOX <victor.pernak@hotmail.fr>
2023-03-23 04:22:27 +0100

    :cyclone: rank update

--- src/main/java/fr/kizafox/pearlanticheat/tools/database/data/RankUnit.kt
+++ src/main/java/fr/kizafox/pearlanticheat/tools/database/data/RankUnit.kt
@@ -2,7 +2,8 @@ package fr.kizafox.pearlanticheat.tools.database.data
 
 enum class RankUnit(val rankName: String) {
 
-    Default("Default");
+    Default("Default"),
+    Moderator("Moderator");
 
     fun getName(): String {
         return this.rankName
