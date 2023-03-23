8946684ad4259cbe824d7f72c169ce51f4ecda7c
KIZAFOX <victor.pernak@hotmail.fr>
2023-03-23 04:41:06 +0100

    :cyclone: constructor & getter added

--- src/main/java/fr/kizafox/pearlanticheat/tools/database/data/UserData.kt
+++ src/main/java/fr/kizafox/pearlanticheat/tools/database/data/UserData.kt
@@ -2,11 +2,7 @@ package fr.kizafox.pearlanticheat.tools.database.data
 
 import java.util.UUID
 
-class UserData(private val id: Int, private val uuid: UUID, private val name: String) {
-
-    fun getId(): Int {
-        return id
-    }
+class UserData(private val uuid: UUID, private val name: String, private val rank: Any?, private val reportCount: Int) {
 
     fun getUUID(): UUID {
         return uuid
@@ -14,5 +10,13 @@ class UserData(private val id: Int, private val uuid: UUID, private val name: St
 
     fun getName(): String {
         return name
+    }
+
+    fun getRank(): RankUnit {
+        return rank as RankUnit
+    }
+
+    fun getReportCont(): Int {
+        return reportCount
     }
 }
